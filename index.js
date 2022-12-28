0964a1f8c228de36abbb1984436336fa5416eae3
cg_venkat <venkatesan.a@capgemini.com>
2022-12-28 11:13:28 +0530

    main

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    AES256: require('./src/AES256'),
+    errorHandler: require('./src/errorHandler'),
+    redisToken: require('./src/redisToken'),
+    redisTokenAdmin: require('./src/redisTokenAdmin'),
+    awsPushSend: require('./src/awsPushSend'),
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+## redisToken, redisTokenAdmin
+- 아래 env 키에 redis서버 정보 입력 필요 (미입력시 localhost 6379)
+- REDIS_HOST
+- REDIS_PORT
